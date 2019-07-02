c12cccf8fe039b462fd1643fb2ded33aa87d3866
shastro <guitarshughes@hotmail.com>
2019-07-01 18:24:52 -0600

    Preparing to Transition to a Generator/Renderer Architecture

---
+++ build/obj/Debug/main.d
@@ -0,0 +1,17 @@
+obj/Debug/main.o: ../src/main.cpp ../include/basic.hpp \
+ ../include/ball.hpp ../include/basic.hpp ../include/utils.hpp \
+ ../include/utils.hpp ../include/sim.hpp ../include/ball.hpp
+
+../include/basic.hpp:
+
+../include/ball.hpp:
+
+../include/basic.hpp:
+
+../include/utils.hpp:
+
+../include/utils.hpp:
+
+../include/sim.hpp:
+
+../include/ball.hpp:
